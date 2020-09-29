020692dec6d3ce3114b9975607d25054e9738a6b
Noor B.A <noor.bahr.aluloom@gmail.com>
2020-09-28 19:07:37 -0700

    wave 3

--- lib/deck.rb
+++ lib/deck.rb
@@ -6,14 +6,26 @@ require_relative 'card'
 class Deck
 
   def initialize
+    @cards = []
+    [:hearts, :spades, :clubs, :diamonds].each do |suit|
+      (1..13).each do |value|
+        @cards << Card.new(value,suit)
+        end
+      end
 
   end
 
   def draw
-    # returns a card
+    drawn_card= @cards.pop
+    return drawn_card
   end
 
   def shuffle
-    # shuffles the deck
+    @cards.shuffle
   end
+
+  def count
+    @cards.length
+  end
+
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,9 +1,55 @@
 require_relative 'test_helper'
 
 describe Deck do
+  before do
+    @deck = Deck.new
+  end
   # Write tests for the Deck class.
   # What should you test?
     # 1.  Expected Behavior
     # 2.  Edge cases
+  describe 'constructor' do
 
+    it "can be instantiated" do
+      expect(@deck).must_be_instance_of Deck
+    end
+  end
+
+  describe "draw method" do
+    it "must responde to draw method" do
+      expect(@deck).must_respond_to :draw
+    end
+
+    it "returns a Card" do
+      expect(@deck.draw).must_be_instance_of Card
+    end
+
+    it "will let me draw 52 cards from an initial deck" do
+      52.times do
+        expect(@deck.draw).must_be_instance_of Card
+      end
+    end
+
+    it "will return nil for an empty deck" do
+      52.times do
+        expect(@deck.draw).must_be_instance_of Card
+      end
+      expect(@deck.draw).must_be_nil
+      end
+  end
+
+  describe "shuffle" do
+    it "responds to shuffle" do
+      expect(@deck).must_respond_to :shuffle
+    end
+
+    describe "count" do
+      it "counts the cards in a deck" do
+        deck = Deck.new
+        expect
+      end
+
+    end
+
+  end
 end
